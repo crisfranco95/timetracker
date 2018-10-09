000ab9c083efaa4fc2dd162023066db5cca80af8
Cristian Franco <cristianjunior22@hotmail.com>
2018-10-09 10:50:55 -0500

    Update auth.js

--- app/routes/v1/auth/auth.js
+++ app/routes/v1/auth/auth.js
@@ -86,41 +86,4 @@ router.post('/register', function (req, res) {
 });
 
 
-// router.get('/current', function (req, res) {
-
-//   var token = req.headers['x-access-token'];
-
-//   if (token) {
-
-//     // verifies secret and checks exp
-//     jwt.verify(token, app.get('secret'), function (err, decoded) {
-
-//       if (err) {
-//         return res.json({
-//           success: false,
-//           message: 'Failed to authenticate token.'
-//         });
-//       } else {
-//         // if everything is good, save to request for use in other routes
-
-//         return res.status(200).send({
-//           success: true,
-//           message: decoded
-//         });
-//       }
-//     });
-
-//   } else {
-//     //if there is no token
-//     //return an error
-//     return res.status(403).send({
-//       success: false,
-//       message: 'No token provided.'
-//     });
-//   }
-
-// });
-
-
-
 module.exports = router;
